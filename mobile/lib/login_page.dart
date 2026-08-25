d2bb7fec872227cb64a8f393d77077e32701ad78
A-Shaarawi <abdelrahmangamal305@gmail.com>
2026-08-25 11:19:46 +0300

    Refactor: Split the login into a new file

---
+++ mobile/lib/login_page.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color.fromARGB(255, 69, 80, 156);
+const Color accentColor = Color(0xFFE91E63);
+const Color forgotPasswordColor = Color.fromARGB(255, 187, 119, 119);
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 80),
+            Text(
+              'Ibnsina Pharma',
+              style: TextStyle(
+                fontSize: 26,
+                fontWeight: FontWeight.bold,
+                color: primaryColor,
+              ),
+            ),
+            Text(
+              "Login to your account",
+              style: TextStyle(fontSize: 12, color: accentColor),
+            ),
+            SizedBox(height: 30),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Phone Number',
+                      labelStyle: TextStyle(
+                        fontSize: 13,
+                        fontWeight: FontWeight(300),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(5)),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 12),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      labelStyle: TextStyle(
+                        fontSize: 13,
+                        fontWeight: FontWeight(300),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(5)),
+                      ),
+                    ),
+                    obscureText: true,
+                  ),
+                  SizedBox(height: 30),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: primaryColor,
+                        foregroundColor: Colors.white,
+                      ),
+                      child: Text('Login'),
+                    ),
+                  ),
+                  SizedBox(height: 50),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Forgot Password?',
+                      style: TextStyle(color: forgotPasswordColor),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
