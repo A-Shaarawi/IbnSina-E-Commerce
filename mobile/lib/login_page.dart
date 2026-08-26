ba2e080031318a0be8fde5e391cac734cac5f2ac
A-Shaarawi <abdelrahmangamal305@gmail.com>
2026-08-26 15:10:13 +0300

    Feat: Handled login button with adding SnackBar errors

--- mobile/lib/login_page.dart
+++ mobile/lib/login_page.dart
@@ -4,8 +4,35 @@ const Color primaryColor = Color.fromARGB(255, 69, 80, 156);
 const Color accentColor = Color(0xFFE91E63);
 const Color forgotPasswordColor = Color.fromARGB(255, 187, 119, 119);
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _phoneController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  void _handleLogin() {
+    final phone = _phoneController.text;
+    final password = _passwordController.text;
+
+    if (phone.isEmpty || password.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Phone number and password are required')),
+      );
+      return;
+    }
+  }
+
+  @override
+  void dispose() {
+    _phoneController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,6 +56,7 @@ class LoginPage extends StatelessWidget {
               child: Column(
                 children: [
                   TextField(
+                    controller: _phoneController,
                     decoration: InputDecoration(
                       labelText: 'Phone Number',
                       labelStyle: TextStyle(
@@ -42,6 +70,7 @@ class LoginPage extends StatelessWidget {
                   ),
                   SizedBox(height: 12),
                   TextField(
+                    controller: _passwordController,
                     decoration: InputDecoration(
                       labelText: 'Password',
                       labelStyle: TextStyle(
@@ -58,7 +87,7 @@ class LoginPage extends StatelessWidget {
                   SizedBox(
                     width: double.infinity,
                     child: ElevatedButton(
-                      onPressed: () {},
+                      onPressed: _handleLogin,
                       style: ElevatedButton.styleFrom(
                         backgroundColor: primaryColor,
                         foregroundColor: Colors.white,
