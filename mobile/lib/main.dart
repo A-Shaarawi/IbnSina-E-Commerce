3ac4564f8680396f7a4f71f9578da0e14faf755e
A-Shaarawi <abdelrahmangamal305@gmail.com>
2026-08-26 14:36:25 +0300

    Feat: Added animated splash screen (using Claude for the animation)

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'login_page.dart';
+import 'splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +11,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: const LoginPage());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const SplashScreen(),
+    );
   }
 }
